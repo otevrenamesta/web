0b4cc71b2dc59b84c32649752591d30d9f479536
vencax <vencax@centrum.cz>
2021-07-19 16:25:23 +0200

    update

---
+++ _service/components/chci.js
@@ -0,0 +1,26 @@
+export default {
+  props: ['data'],
+  template: `
+<div class="columns mydarkblue mycontact">
+  <div class="column p-6">
+    <h2 class="title is-4">Chci řešení na míru</h2>
+    <div class="content">
+      Potřebuji poradit, chci objednat jen jednu z aplikací nebo služeb. 
+      Neváhejte se na nás obrátit s Vaším dotazem.
+
+      <button>[vyvoj@otevrenamesta.cz](mailto:vyvoj@otevrenamesta.cz)</button>
+    </div>
+  </div>
+
+  <div class="column p-6">
+    <h2 class="title is-4">Chci se stát členem</h2>
+    <div class="content">
+      Neváhejte se zeptat, jak se stát členem naší organizace 
+      a napomoci tak digitalizace Vaší obce.
+
+      <button>[clenstvi@otevrenamesta.cz](mailto:clenstvi@otevrenamesta.cz)</button>
+    </div>
+  </div>
+</div>
+  `
+}
---
+++ _service/components/projectIndex.js
@@ -0,0 +1,36 @@
+
+export default {
+  data: function () {
+    return {
+      items: null
+    }
+  },
+  created: async function () {
+    try {
+      const dataReq = await axios.get(this.$props.data.url)
+      this.$data.items = dataReq.data
+    } catch (_) {
+      this.$data.posts = [{ title: 'newsPreview: asi spatne url v datech' }]
+    }
+  },
+  props: ['data'],
+  template: `
+    <div class="columns is-flex-wrap-wrap">
+      <div v-for="(i, idx) in items" :key="idx" class="column is-4">
+        <figure class="image is-4by3">
+          <img :src="$store.getters.mediaUrl(i.obrazek, 'w=640')" :alt="i.title">
+        </figure>
+
+        <h3 class="title red">{{ i.title }}</h3>        
+        <h4 class="subtitle brown">{{ i.subtitle }}</h4>
+
+        <router-link :to="'/projekty/' + i.id">
+          <button class="button is-primary">
+            Zjistit více <i class="fas fa-angle-right"></i>
+          </button>
+        </router-link>
+
+      </div>
+    </div>
+  `
+}

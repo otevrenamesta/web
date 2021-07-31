28a12ea8125e68f0ce834b4cea86f2d633e156dc
vencax <vencax@centrum.cz>
2021-07-31 22:59:23 +0200

    contactform update

--- _service/components/contactform.js
+++ _service/components/contactform.js
@@ -14,7 +14,21 @@ const formComponents = {
       :value="data[cfg.name]"
       @input="evt => data[cfg.name] = evt.target.value" />
     `
-  }
+  },
+  fselect: {
+    props: [ 'data', 'cfg' ],
+    template: `
+    <div class="select">
+      <select 
+        :value="data[cfg.name]"
+        @input="evt => data[cfg.name] = evt.target.value"
+      >
+        <option :value="null">---</option>
+        <option v-for="i,idx in cfg.options" :key="idx" :value="i.value">{{ i.text }}</option>
+      </select>
+    </div>
+    `
+  },
 }
 
 export default {
@@ -47,14 +61,17 @@ export default {
     handleSubmit: async function () {
       this.$data.submitting = true
       try {
-        const data = Object.assign({ id: this.$data.qid, to: this.$props.data.to }, this.$data.formdata)
+        const data = Object.assign({ 
+          id: this.$data.qid, 
+          url: this.$props.data.api 
+        }, this.$data.formdata)
         const res = await axios.post(this.$props.data.url, data)
       } catch (err) {
         const message = 'nesouhlasí odpověď na kontrolní otázku'
         this.$store.dispatch('toast', { message, type: 'error' })
       } finally {
         this.$data.submitting = false
-      }      
+      }
     }
   },
   props: ['data'],
@@ -62,21 +79,27 @@ export default {
   template: `
 <form @submit.prevent="handleSubmit">
 
-  <div v-if="question" class="field" v-for="i, idx in formcontrols">
-    <label class="label">{{ i.label }}</label>
-    <div class="control">
-      <component :is="'f' + i.component" 
-        :data="formdata" :cfg="i"
-        :placeholder="i.placeholder" />
+  <div v-if="question" class="columns is-flex-wrap-wrap">
+    <div class="column" :class="i.class" v-for="i, idx in formcontrols" :key="idx">
+      <div class="field">
+        <label class="label">{{ i.label }}</label>
+        <div class="control">
+          <component :is="'f' + i.component" 
+            :data="formdata" :cfg="i"
+            :placeholder="i.placeholder" />
+        </div>
+      </div>
     </div>
   </div>
 
   <div class="field is-grouped">
     <div class="control">
-      <button type="submit" class="button is-link" 
-        :disabled="submitting"
-      >Odeslat</button>
+      <button type="submit" class="button is-link" :disabled="submitting">
+        <span class="icon is-small"><i class="fas fa-paper-plane"></i></span>
+        <span>Odeslat</span>
+      </button>
     </div>
+    <span>{{ data.extratext }}</span>
   </div>
 
 <form>
